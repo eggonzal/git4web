2f774b789fb69a4d9c7348966ce5bd053b6f915f
Gibran Gonzalez <haibrayn@hotmail.com>
2017-05-25 21:17:11 -0500

    Removed DEBUG and undefined guards
    Trying to keep the code clean and trusting that the arguments won't be
    undefined elements.

--- Mozilla Extensions/git4web/UI_selector.js
+++ Mozilla Extensions/git4web/UI_selector.js
@@ -1,6 +1,5 @@
 selector = function () {
 	// Private things o.o
-	var DEBUG = false;
 	var _selectedElement = {};
 
 	// state
@@ -25,16 +24,13 @@ selector = function () {
 		document.body.appendChild(css);
 	};
 
-	// Init
 	var init = function () {
-		if(DEBUG) console.log('On init'); 
 		_createSelectorStyleSheet();
 		_addMouseOverListener();
 		_start();
 	};
 
 	var _addMouseOverListener = function () {
-		if(DEBUG) console.log('Adding mouseover listener'); 
 		document.body.addEventListener("mouseover", function (e) {
 			if (!_isRunning())
 				return;
@@ -54,47 +50,21 @@ selector = function () {
 		return !_targetHasClass(target, _SELECTOR_CLASS);
 	};
 	var _addEventsToTarget = function (target) {
-		if (DEBUG) 
-			console.log('Adding Events');
-		
-		_addClickEventToTarget(target);
-		_addMouseoutEventToTarget(target);
+		target.addEventListener("click", _clickEvent);
+		target.addEventListener("mouseout", _mouseoutEvent);
 	};
 	var _canAddClassToTarget = function (target, classToAdd) {
-		if(DEBUG) console.log('Can add class to target?' + _targetHasClass(target, classToAdd));
 		return !_targetHasClass(target, classToAdd);
 	};
 	var _targetHasClass = function (target, className) {
 		return target.className.indexOf(className) >= 0;
 	};
 	var _addClassToTarget = function (target, classToAdd) {
-		if (target === undefined || target.className === undefined)
-			return console.log('No target to add class name.');
-		if (DEBUG) 
-			console.log('Adding class to target');
-		
 		target.className += classToAdd === undefined ? "" : classToAdd;
 	};
-	
-	//-- --
-	var _addClickEventToTarget = function (target) {
-		if (DEBUG) 
-			console.log('Adding click event');
-		
-		target.addEventListener("click", _clickEvent);
-	};
-	var _addMouseoutEventToTarget = function (target) {
-		if (DEBUG) 
-			console.log('Adding mouse out event');
-		
-		target.addEventListener("mouseout", _mouseoutEvent);
-	};
 
-	//-- -- --
+	//-- --
 	var _mouseoutEvent = function (e) {
-		if(DEBUG)
-			console.log('On mouseout event');
-		
 		_clearTarget(e.target);
 	};
 	var _clickEvent = function (e) {
@@ -108,33 +78,26 @@ selector = function () {
 		_clearTarget(e.target);
 		_stop();
 	};
-
-	//-- -- -- --
+	
+	//-- -- --
 	var _selectTarget = function (target) {
 		_selectedElement = target;
 	};
 	var _clearTarget = function (target) {
-		if(DEBUG) console.log('On Clear Target'); 
 		_removeEventsFromTarget(target);
 		_removeClassFromTarget(target, _SELECTOR_CLASS);
 	};
 
-	//-- -- -- -- --
+	//-- -- -- --
 	var _removeEventsFromTarget = function (target) {
-		if(DEBUG) console.log('Removing Events from target'); 
 		target.removeEventListener("click", _clickEvent);
 		target.removeEventListener("mouseout", _mouseoutEvent);
 	};
-	var _removeClassFromTarget = function (target, classToRemove) {
-		if(DEBUG) console.log('Removing class from target'); 
-		if (target === undefined || target.className === undefined || target.className.replace === undefined)
-			return console.log('No target to remove class name.');
-		
+	var _removeClassFromTarget = function (target, classToRemove) {		
 		target.className = target.className.replace(new RegExp(classToRemove + "\\b", "g"), "");
 	};
 
 	var createSelectorObject = function () {
-		if(DEBUG) console.log('On create object'); 
 		init();
 		return {
 			start: function () {
